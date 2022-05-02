4ca2ba6a553f6e261c843381ea531f7e1c492b5b
hope140 <hope140y@outlook.com>
2022-05-02 09:52:06 +0800

    🛹  Add upgrade script

--- upgrade.js
+++ upgrade.js
@@ -1,35 +1,4 @@
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-async function lianti(userid, offlinenum, maxrun) {
-	for (let count = 0; count < maxrun; count++) {
-		await sleep(4200)
-		console.log("第" + (count + 1) + "次练体");
-		var request = require('request');
-		var options = {
-			'method': 'POST',
-			'url': 'https://yqxxl.yqbros.com/Yqxxl/Map/liantiStart',
-			'headers': {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				"userId": userid,
-				"offlineNum": offlinenum
-			})
-		};
-		request(options, function (error, response) {
-			// if (error) throw new Error(error);
-			msg = JSON.parse(response.body);
-			if (msg.code == 0) {
-				console.log(msg.data.msg + " 气血：" + msg.data.userStateInfo.hp + "/" + msg.data.userStateInfo.hpMax + " 灵：" + msg.data.userStateInfo.linMp + "/" + msg.data.userStateInfo.linMpMax);
-			} else if (msg.code == -3) {
-				console.log("请求超时，同时运行多个脚本或游戏未退出，请检查！");
-			} else {
-				console.log(msg.msg);
-			}
-			// console.log(msg);
-		});
-	}
-	task.next('炼体结束，开始下一步');
-}
 async function dazuo(userid, mapname, mapx, mapy, maxrun) {
 	for (let count = 0; count < maxrun; count++) {
 		await sleep(4200)
@@ -77,6 +46,31 @@ async function dazuo(userid, mapname, mapx, mapy, maxrun) {
 	task.next('打坐结束，开始下一步');
 }
 
+async function upgrade(userid, type) {
+	var request = require('request');
+	var options = {
+		'method': 'POST',
+		'url': 'https://yqxxl.yqbros.com/Yqxxl/User/upgradeLv',
+		'headers': {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			"userId": userid,
+			"type": type
+		})
+	};
+	request(options, function (error, response) {
+		// if (error) throw new Error(error);
+		msg = JSON.parse(response.body);
+		if (msg.code == 0) {
+			console.log(msg.data.msg + msg.data.userLv.wuLV + "级武者" + msg.data.userLv.linLv + "级灵者" + msg.data.userLv.hunLv + "级炼药师");
+		} else {
+			console.log(msg.msg);
+		}
+	});
+	task.next('升级结束，开始下一步');
+}
+
 // async function main() {
 // 	collection(27188, "炎谷", 7, 8, 0, 27);
 // 	await sleep(4200);
@@ -86,8 +80,8 @@ async function dazuo(userid, mapname, mapx, mapy, maxrun) {
 
 async function* main() {
 	for (let count = 0; count < 1000; count++) {
-		yield dazuo(27188, "炎谷", 7, 8, 37);
-		yield lianti(27188, 0, 80);
+		yield upgrade(27188, 2);
+		yield dazuo(27188, "炎谷", 7, 8, 25);
 	}
 }
 const task = main()
